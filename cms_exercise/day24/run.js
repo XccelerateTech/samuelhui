dec9152ae6e54eb7fc9118fe1b185df37c718ad9
saboshi69 <42663794+saboshi69@users.noreply.github.com>
2018-11-01 19:37:28 +0800

    day24

--- cms_exercise/day24/run.js
+++ cms_exercise/day24/run.js
@@ -1,25 +1,32 @@
 let fs = require('fs');
 
-function checkPath(curPath){
+let checkPath = function(curPath){
     return new Promise((resolve, reject)=>{
         fs.readdir(`${__dirname}/${curPath}`, function(err, files){
             if (err){
-                console.log("somewrong")
+                console.log("wrong path")
                 reject()
             }else{
-                console.log("successfully get some paths")
-                resolve(files, curPath)
+                for (let i = 0; i < files.length; i++){
+                    fs.stat(`${__dirname}/${curPath}/${files[i]}`, (err, stats)=>{
+                        if(err){
+                            console.log("cant read files status: "+err)
+                        }else if(stats.isFile()){
+                            console.log(`${curPath}/${files[i]} is file`)                           
+                        }else{
+                            console.log(`${curPath}/${files[i]} is directory`)
+                            checkPath(`${curPath}/${files[i]}`)                           
+                        }
+                    })
+                }
+                
             }
         })
+        resolve()
     })
 }
 
-function isPath(files){
-    return new Promise((resolve, reject)=>{
-    checkPath( curPath + files[0] )
-    })
-}
+module.exports = checkPath
 
 
-checkPath("files")
-.then
+
---
+++ cms_exercise/day24/test.js
@@ -0,0 +1,5 @@
+let checkpath = require("./run")
+
+
+checkpath("files")
+.then(()=>console.log("check completed"))
