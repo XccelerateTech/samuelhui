578b49f6ee0e08f606d8b2d356e8d9b5ee5453ab
saboshi69 <42663794+saboshi69@users.noreply.github.com>
2018-11-10 18:06:39 +0800

    somePoop

--- cms_exercise/day30wEx/stickyActions.js
+++ cms_exercise/day30wEx/stickyActions.js
@@ -3,30 +3,60 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 
-class StickyActions{
-    constructor(inputYourJsonFile){   
-        this.path = inputYourJsonFile;    
+class StickyActions {
+    constructor(inputYourJsonFile) {
+        this.path = inputYourJsonFile;
     }
-    readJson(){             // this function return a message json file
-        return new Promise((resolve,reject)=>{
-            fs.readFile(`./json/${this.path}`,(err, data)=>{
-                if(err){
+    readJson() { // this function return a message json file
+        return new Promise((resolve, reject) => {
+            fs.readFile(`./json/${this.path}`, 'utf8', (err, data) => {
+                if (err) {
                     console.log('cannot find message.json')
-                }else{
-                    console.log(`successful get: ${this.path}`)                
+                } else {
+                    console.log(`successful get: ${this.path}`)
                     resolve(JSON.parse(data))
                 }
             })
         })
     }
 
-    // writeJson(data){    
-    //     return new Promise((resolve,reject)=>{
-    //         let newMsg = data;
-    //         fs.writeFile(`./json/${this.path}`, newMsg,)
-    //     })
+    addMsg(data, user, stuff) {
+        return new Promise((resolve, reject) =>{
+            let newNotes = data[user]["msg"]
+            data[user]["msg"] =[stuff,...newNotes]
+            resolve(data)
+        })
+    }
 
-    // }
+    delMsg(data, user, msgIndex){
+        return new Promise((resolve, reject)=>{
+            let r = []
+            let l = data[user]["msg"].length
+            for(let i = 0; i < l; i ++){
+                if(i != msgIndex){
+                  r.push(data[user]["msg"][i])
+                }
+            }
+            data[user]["msg"] = r
+            resolve(data)
+        })
+    }
+
+
+
+    wrtieJson(data) { // this function return a message json file
+        return new Promise((resolve, reject) => {
+            let latestData = JSON.stringify(data)
+            fs.writeFile(`./json/${this.path}`, latestData, (err, data) => {
+                if (err) {
+                    console.log('cannot write message.json')
+                } else {
+                    console.log(`successfully replace: ${this.path}`)
+                    resolve(latestData)
+                }
+            })
+        })
+    }
 
 }
 
